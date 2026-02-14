9b848b6d006e4e6b187643f4bb5a2703bc6a82ae
lamat <lamathesonrobotics@gmail.com>
2026-02-13 16:36:05 -0800

    Updates - Distance Sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NEWRO/Auto/OffLine.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.NEWRO.Auto;
+
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous
+public final class OffLine extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        waitForStart();
+
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(0, 0, 0))
+                        .lineToX(30)
+                        .build());
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NEWRO/Testing/DistanceTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.NEWRO.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.NEWRO.Processors.DistanceProcessor;
+
+@TeleOp
+public class DistanceTest extends OpMode {
+
+    DistanceProcessor Distance = new DistanceProcessor();
+
+    @Override
+    public void init() {
+        Distance.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Distance From Ball: ", Distance.getDistance());
+    }
+}
